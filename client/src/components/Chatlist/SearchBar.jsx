734dba58792b749cb1624d509f942a2c6626f0e9
unknown <haldersoham46@gmail.com>
2023-11-04 17:46:12 +0530

    try

--- client/src/components/Chatlist/SearchBar.jsx
+++ client/src/components/Chatlist/SearchBar.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import {BiSearchAlt2} from 'react-icons/bi'
+import React from "react";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { BsFilter } from "react-icons/bs"
 const SearchBar = () => {
   return (
-    <div className={'bg-search-input-container-background flex py-3 px-5 items-center gap-3 h-16 '} >
-    <div className={'bg-panel-header-background flex items-center gap-5 px-3 py-3 rounded-lg flex-grow'} >
-    <div> 
-    <BiSearchAlt2 className={'text-panel-header-icon cursor-pointer text-lg'} />
+    <div
+      className={
+        "bg-search-input-container-background flex py-3 px-5 items-center gap-3 h-16 "
+      }>
+      <div
+        className={
+          "bg-panel-header-background flex items-center gap-5 px-3 py-3 rounded-lg flex-grow"
+        }>
+        <div>
+          <BiSearchAlt2
+            className={"text-panel-header-icon cursor-pointer text-lg"}
+          />
+        </div>
+        <div className={'w-[90%]'} >
+          <input
+            type={"text"}
+            placeholder={"Search or start a new Chat"}
+            className={
+              "bg-transparent text-sm focus:outline-none text-white w-full"
+            }
+          />
+        </div>
+      </div>
+      <div >
+      <BsFilter className={"text-panel-header-icon cursor-pointer text-lg"} />
+      </div>
     </div>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
