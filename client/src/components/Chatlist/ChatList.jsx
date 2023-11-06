06872d4d727b7452b59a3be1f572f1c9bc0c9686
unknown <haldersoham46@gmail.com>
2023-11-06 19:01:16 +0530

    Try

--- client/src/components/Chatlist/ChatList.jsx
+++ client/src/components/Chatlist/ChatList.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import ChatListHeader from './ChatListHeader'
-import SearchBar from './SearchBar'
-import List from './List'
+import React, { useState, useEffect } from "react";
+import ChatListHeader from "./ChatListHeader";
+import SearchBar from "./SearchBar";
+import List from "./List";
+import { useStateProvider } from "@/context/StateContext";
+import ContactList from "./ContactList";
 
 const ChatList = () => {
+  const [{ contactsPage }] = useStateProvider();
+  const [pageType, setPageType] = useState("default");
+  useEffect(() => {
+    setPageType(pageType ? "all-contacts" : "default");
+  }, [contactsPage]);
   return (
-    <div className={'bg-panel-header-background flex flex-col max-h-screen z-20'} >
-    <>
-        <ChatListHeader />
-        <SearchBar />
-        <List />
-    </>
+    <div
+      className={"bg-panel-header-background flex flex-col max-h-screen z-20"}>
+      {pageType === "default" && (
+        <>
+          <ChatListHeader />
+          <SearchBar />
+          <List />
+        </>
+      )}
+      {pageType === "all-contacts" && <ContactList />}
     </div>
-  )
-}
+  );
+};
 
-export default ChatList
+export default ChatList;
--- client/src/context/StateReducers.jsx
+++ client/src/context/StateReducers.jsx
@@ -1,21 +1,25 @@
 import { reducerCases } from "./constants";
 
 export const initialState = {
-    userInfo: undefined,
-    newUser: false,
-}
+  userInfo: undefined,
+  newUser: false,
+  contactsPage: false,
+};
 
-const reducer = (state, action) =>{
-    switch (action.type) {
-        case reducerCases.SET_USER_INFO:
-            return {
-                ...state, userInfo: action.userInfo,
-            }
-        case reducerCases.SET_NEW_USER:
-            return {...state, newUser: action.newUser}
-        default:
-            return state;
-    }
-}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case reducerCases.SET_USER_INFO:
+      return {
+        ...state,
+        userInfo: action.userInfo,
+      };
+    case reducerCases.SET_NEW_USER:
+      return { ...state, newUser: action.newUser };
+    case reducerCases.SET_ALL_CONTACT_PAGE:
+      return { ...state, contactsPage: !state.contactsPage };
+    default:
+      return state;
+  }
+};
 
-export default reducer
+export default reducer;
